2d81fa029beb6c63c0d124127f27ac4df8e733e7
dcode-fullstack <dcode.fullstack@gmail.com>
2022-04-04 18:40:16 +0200

    js finished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select Tab Content Item
+function selecItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add("tab-border");
+    // Grab Content Item from the DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove("show"));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener("click", selecItem));
